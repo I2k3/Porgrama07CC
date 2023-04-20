e2c90f80388d9fcd442445eb07f949b56fb7f6d2
I2k3 <itzel5kari10@gmail.com>
2023-04-20 14:17:41 -0600

    Add Practica07CC

---
+++ src/main/java/org/uv/programa07cc/IDAOGeneral.java
@@ -0,0 +1,18 @@
+package org.uv.programa07cc;
+
+import java.util.List;
+
+/**
+ *
+ * @author Itzel Rios
+ */
+
+
+public interface IDAOGeneral<T, ID>{
+    public T create(T p);
+    public boolean delete(ID id);
+    public T update(ID id, T p);
+    
+    public List<T> findAll();
+    public T findById(ID id);
+}
